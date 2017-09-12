a55278b615dbfbe73540753939415fa9acb65cdf
Richardfan <fanruchao@bupt.edu.cn>
2017-09-12 15:53:09 +0800

    add visdom during training

--- train.py
+++ train.py
@@ -12,6 +12,7 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 import time 
+import numpy as np
 
 
 def train(model, train_loader, loss_fn, optimizer, print_every=10):
@@ -52,15 +53,16 @@ def train(model, train_loader, loss_fn, optimizer, print_every=10):
     average_loss = total_loss / i
     print("Epoch done, average loss: %.4f" % average_loss)
     logger.info("Epoch done, average loss: %.4f" % average_loss)
+    return average_loss
 
-def dev(model):
+def dev(model, data):
     model.eval()
     total_cer = 0
     total_tokens = 0
     decoder  = Decoder("#'acbedgfihkjmlonqpsrutwvyxz_", space_idx=28, blank_index=0)
     
-    dev_dataset = myDataset(data_set='dev', n_mfcc=39)
-    dev_loader = myDataLoader(dev_dataset, batch_size=4, shuffle=False,
+    dev_dataset = myDataset(data_set=data, feature_type="fbank", n_feats=40)
+    dev_loader = myDataLoader(dev_dataset, batch_size=8, shuffle=False,
                     num_workers=4, pin_memory=False)
 
     for data in dev_loader:
@@ -97,65 +99,80 @@ def main(init_lr):
     log_file = './log/train.log'
     logger = init_logger(log_file)
     
-    params = dict()
-    params['rnn_input_size'] = 39
-    params['rnn_hidden_size'] = 256
-    params['rnn_layers'] = 5
-    params['rnn_type'] = nn.GRU
-    params['num_class'] = 28
-    params['bidirectional'] = True
-    params['batch_norm'] = True
-
-    params['num_epoches'] = 40
-    params['least_train_epoch'] = 15
+    from visdom import Visdom
+    viz = Visdom()
+    opts = [dict(title="Timit Fbank40"+" Loss", ylabel = 'Loss', xlabel = 'Epoch'),
+            dict(title="Timit Fbank40"+" CER on Train", ylabel = 'CER', xlabel = 'Epoch'),
+            dict(title='Timit Fbank40'+' CER on DEV', ylabel = 'DEV CER', xlabel = 'Epoch')]
+    viz_window = [None, None, None]
+    
+    num_epoches = 40
+    least_train_epoch = 20
     end_adjust_acc = 0.05
-    params['decay'] = 0.1
     decay = 0.5
     count = 0
-    params['learning_rate'] = init_lr
-    params['batch_size'] = 4
     learning_rate = init_lr
+    batch_size = 8
+    weight_decay = 0.0001
+    
+    params = { 'num_epoches':num_epoches, 'least_train_epoch':least_train_epoch, 'end_adjust_acc':end_adjust_acc,
+            'decay':decay, 'learning_rate':init_lr, 'weight_decay':weight_decay, 'batch_size':batch_size }
     
     acc_best = -100
     adjust_rate_flag = False
     stop_train = False
 
-    model = CTC_RNN(rnn_input_size=params['rnn_input_size'], rnn_hidden_size=params['rnn_hidden_size'], rnn_layers=params['rnn_layers'], 
-                    rnn_type=params['rnn_type'], bidirectional=params['bidirectional'], batch_norm=params['batch_norm'], 
-                    num_class=params['num_class'])
+    model = CTC_RNN(rnn_input_size=40, rnn_hidden_size=256, rnn_layers=4, 
+                    rnn_type=nn.LSTM, bidirectional=True, batch_norm=True, 
+                    num_class=28, drop_out = 0)
     if USE_CUDA:
         model = model.cuda()
-    
     loss_fn = CTCLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=init_lr)
+    optimizer = torch.optim.Adam(model.parameters(), lr=init_lr, weight_decay=weight_decay)
     
+    adjust_time = 0
     start_time = time.time()
+    loss_results = []
+    training_cer_results = []
+    dev_cer_results = []
+    
     while not stop_train:
-        if count >= params['num_epoches']:
+        if count >= num_epoches:
             break
         count += 1
         print("Start training epoch: %d" % count)
         logger.info("Start training epoch: %d" % count)
+        
         if adjust_rate_flag:
             learning_rate *= decay
             for param in optimizer.param_groups:
                 param['lr'] *= decay
 
-        train_dataset = myDataset(data_set='train', n_mfcc=39)
-        train_loader = myDataLoader(train_dataset, batch_size=4, shuffle=True,
+        train_dataset = myDataset(data_set='train', feature_type="fbank", n_feats=40)
+        train_loader = myDataLoader(train_dataset, batch_size=batch_size, shuffle=True,
                     num_workers=4, pin_memory=False)
-        train(model, train_loader, loss_fn, optimizer, print_every=20)
-        acc = dev(model)
+        loss = train(model, train_loader, loss_fn, optimizer, print_every=20)
+        loss_results.append(loss)
+        cer = dev(model, 'train')
+        print("cer on training set is %.4f" % cer)
+        logger.info("cer on training set is %.4f" % cer)
+        training_cer_results.append(cer)
+        acc = dev(model, 'dev')
+        dev_cer_results.append(acc)
+        
         model_path_accept = './log/epoch'+str(count)+'_lr'+str(learning_rate)+'_cv'+str(acc)+'.pkl'
         model_path_reject = './log/epoch'+str(count)+'_lr'+str(learning_rate)+'_cv'+str(acc)+'_rejected.pkl'
+        
+        if adjust_time == 2:
+            stop_train = True
         
         ##15轮迭代之后，开始调整学习率
         if count >= params['least_train_epoch']:
             model_state = model.state_dict()
             op_state = optimizer.state_dict()
-            adjust_rate_flag = True
 
             if acc > (acc_best + end_adjust_acc):            
+                adjust_rate_flag = False
                 acc_best = acc
                 torch.save(model_state, model_path_accept)
             elif (acc > acc_best):
@@ -163,6 +180,8 @@ def main(init_lr):
                 acc_best = acc
                 torch.save(model_state, model_path_accept)
             elif (acc <= acc_best):
+                adjust_rate_flag = True
+                adjust_time += 1
                 torch.save(model.state_dict(), model_path_reject)
                 model.load_state_dict(model_state)
                 optimizer.load_state_dict(op_state)
@@ -170,22 +189,19 @@ def main(init_lr):
         time_used = (time.time() - start_time) / 60
         print("epoch %d done, cv acc is: %.4f, time_used: %.4f minutes" % (count, acc, time_used))
         logger.info("epoch %d done, cv acc is: %.4f, time_used: %.4f minutes" % (count, acc, time_used))
+        x_axis = range(count)
+        y_axis = [loss_results[0:count], training_cer_results[0:count], dev_cer_results[0:count]]
+        for x in range(len(viz_window)):
+            if viz_window[x] is None:
+                viz_window[x] = viz.line(X = np.array(x_axis), Y = np.array(y_axis[x]), opts = opts[x],)
+            else:
+                viz.line(X = np.array(x_axis), Y = np.array(y_axis[x]), win = viz_window[x], update = 'replace',)
+
     print("End training, best cv acc is: %.4f" % acc_best)
     logger.info("End training, best cv acc is: %.4f" % acc_best)
     best_path = './log/best_model'+'_cv'+str(acc_best)+'.pkl'
-    torch.save(model_state, best_path)
-    params['acc best for dev']=acc_best
-    result_record(params)
-
-def result_record(params):
-    record_file = 'training_record_for_timit'
-    date = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
-    f = open(record_file, 'a+')
-    f.writelines("Experiment "+date+':\n')
-    for x in params:
-        f.writelines('\t'+x+': '+str(params[x])+'\n')
-    f.close()
+    params['epoch']=count
+    torch.save(CTC_RNN.save_package(model, optimizer=optimizer, epoch=params, loss_results=loss_results, training_cer_results=training_cer_results, dev_cer_results=dev_cer_results), best_path)
 
 if __name__ == '__main__':
     main(0.001)
-
