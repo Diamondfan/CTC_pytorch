36812e56059ff6cfc8702a79672134710ca49274
Richardfan <fanruchao@bupt.edu.cn>
2018-01-04 16:18:55 +0800

    re-organize the code

--- timit/run.sh
+++ timit/run.sh
@@ -9,8 +9,7 @@ stage=0
 decode_type='Greedy'
 data_set='test'
 lm_path='./data_prepare/data/lm_train.text'
-lstm_ctc_CONF_FILE='./conf/lstm_ctc_setting.conf'
-cnn_lstm_ctc_CONF_FILE='./conf/cnn_lstm_ctc_setting.conf'
+CONF_FILE='./conf/ctc_model_setting.conf'
 LOG_DIR='./log/'
 
 if [ ! -z $1 ]; then
@@ -22,14 +21,12 @@ if [ $stage -le 0 ]; then
     echo "                     Training                         "
     echo ========================================================
 
-    python steps/cnn_lstm_ctc.py --conf $cnn_lstm_ctc_CONF_FILE --log-dir $LOG_DIR || exit 1;
-    #python steps/lstm_ctc.py --conf $lstm_ctc_CONF_FILE --log-dir $LOG_DIR
+    python steps/ctc_train.py --conf $CONF_FILE --log-dir $LOG_DIR || exit 1;
 fi
 
 echo ========================================================
 echo "                 Greedy Decoding                      "
 echo ========================================================
 
-python steps/test.py --conf $cnn_lstm_ctc_CONF_FILE --decode-type $decode_type --map-48-39 ./decode_map_48-39/map_dict.pkl --data-set $data_set --lm-path $lm_path
-#python steps/test.py --conf $lstm_ctc_CONF_FILE --decode-type 'Greedy' --map-48-39 ./decode_map_48-39/map_dict.pkl --data-set 'test'
+python steps/test.py --conf $CONF_FILE --decode-type $decode_type --map-48-39 ./decode_map_48-39/map_dict.pkl --data-set $data_set --lm-path $lm_path
 
